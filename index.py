a97ceda66e48c380e0cff0233e494ca8a6e6560b
Kauan-Luiz <kl321491@gmail.com>
2024-08-15 12:23:51 -0300

    voltando a fazer do 0

--- index.py
+++ index.py
@@ -1,29 +1,22 @@
 from tkinter import *
 from tkinter import ttk
 
-
-
+cor1 = "#1e1f1e" #black
+cor2 = "#fefff" #white
+cor3 = "#38576b" #blue
+cor4 = "#ECEFF1" #grey
+cor5 = "#FFAB40" #orange
 window = Tk()
 window.title("Calculadora")
-window.geometry("350x300")
+window.geometry("235x318")
+window.config(bg=cor1)
 
-resultframe = Frame(window,width=330, height=80, bg="black" )
-framebuttons = Frame(window, width=350,  height=200, bg="black")
+frameresult = Frame(window, width=235, height=50, bg=cor4)
+framebuttons = Frame(window, width=235,  height=200, bg="black")
 
-resultframe.pack(side=TOP, padx=10, pady=10)
-framebuttons.pack(side=LEFT, padx=10, pady=10)
+frameresult.grid(row=0, column=0, pady="10")
+framebuttons.grid(row=1, column=0)
 
-buttons = [
-    ('7', 1, 0), ('8', 1, 1), ('9', 1, 2), ('/', 1, 3),
-    ('4', 2, 0), ('5', 2, 1), ('6', 2, 2), ('*', 2, 3),
-    ('1', 3, 0), ('2', 3, 1), ('3', 3, 2), ('-', 3, 3),
-    ('0', 4, 0), ('.', 4, 1), ('+', 4, 2), ('=', 4, 3),
-    ('C', 5, 0)
-]
-
-for (text, row, col) in buttons:
-    button = ttk.Button(framebuttons, text=text, width=15, height=2)
-    button.grid(row=row, column=col)
 
 window.mainloop() 
 
